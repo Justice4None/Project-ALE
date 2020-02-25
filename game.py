c40e9115df7528e04da89204c2aa79bc737a5f39
William Dunn <pikablu112@gmail.com>
2020-02-25 00:48:11 -0800

    Added player info classes

--- game.py
+++ game.py
@@ -70,7 +70,7 @@ class player_attributes:
         # Affects every "dice roll" in the game.
 
 
-class player_skills:
+class player_skills_strength:
     def __init__(self):
         self.acrobatics = 0,
         # The Acrobatics skill involves jumping, climbing, and avoiding damage from falls. The higher one's Acrobatics, the higher one can jump vertically, the farther one can jump horizontally, and the greater the height one may fall from (or jump down on purpose). Those skilled in Acrobatics can reach areas others cannot get to. With Acrobatics pushed by magic to abnormally high levels, one can jump and fall rather extreme distances.
@@ -82,6 +82,34 @@ class player_skills:
         # The Axe skill helps your character wield heavy chopping weapons like war axes and battleaxes more effectively.
         self.blunt = 0,
         # The Blunt skill makes your character more effective when using bashing weapons like maces, hammers, clubs or staves.
-        self.longblade = 0,
+        self.longblade = 0
         # The Long Blade skill lets your character use broadswords, sabers, longswords, claymores, katanas and dai-katanas effectively.
-        self.
+
+
+class player_skills_intelligence:
+    def __init__(self):
+        self.alchemy = 0,
+        # Alchemy allows your character to identify magical properties in ingredients and create potions to generate useful temporary or onetime effects, such as healing, cure disease, water-walking, mana shielding, and fortifying body attributes. A higher Alchemy skill allows you to see more effects of ingredients when you inspect them.
+        # The higher your character's Alchemy skill is, the more successful and powerful your created potions will be. You will also gather more ingredients with a higher Alchemy skill, and you will be albe to substitute certain ingredients for eachother.
+        self.conjuration = 0,
+        # Conjuration allows your character to dominate the minds of mundane and magical creatures, summon otherworldly weapons and armour, and occasionally, beings.
+        # Conjuration also lets your character delve into the realm of Necromancy. If your character's mortal soul is damaged, or "wounded", enough, they can eclipse the mortal world and become a Lich. Liches cannot move in daylight, nor can they directly harm mortals. Liches can interact through the world with undead serveants and mortal worshippers.
+        self.enchant = 0,
+        # Enchanting is the process of permanently applying a magical effect to an item by harnessing the power of either the Aether or Abyss. This skill governs the creation, use, and recharging of enchanted items. Skilled enchanters are more likely to be successful at creating new items, their enchanted items use less power and are recharged more efficiently.
+        self.security = 0,
+        # The Security skill lets your character open locked doors and containers with lockpicks or wax keys, disarm traps with disarming kits or probes, and re-seal sealed documents after reading them. This skill is essential for agents and thieves alike. Unlocking a locked object is a crime, so make sure you're alone before you go snooping around in places you don't belong.
+        self.medical = 0
+        # The Medical skill lets your character diagnose and treat wounds and diseases without specific items or materials (i.e., medical kits and bandages). This skill is automatically used when your character rests for the night, but only if they sleep in a proper bed or have enough friendly/allied NPC's around.
+
+
+class player_skills_willpower:
+    def __init__(self):
+        self.alteration = 0,
+        # Alteration allows your character to manipulate the physical world and its natural properties. Alteration effects include water breathing and walking, jumping, levitating, burdening, opening and locking, and creating shield barriers against both physical and magical damage.
+        self.destruction = 0,
+        # Destruction allows your character to harm living and unliving things with magick. This usually included elemental damaged, draining, vulnerability, and disintegration effects.
+        self.mysticism = 0,
+        # Mysticism allows your character to shape and focus otherworldly forces to teleport the caster's body, manipulate the world with telekinesis, absorb or reflect magical energies, and sence unsee objects at a distance.
+        # Mysticism is also the art of fortune-telling and future-reading. While used sparcely, this ability may come in handy...
+        self.restoration = 0,
+        #
